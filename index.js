cdc0b81248d9822caa351c6e8ddb7c5fa6bdc223
Aditya Kumar Alok <officialadityakumaralok@gmail.com>
2021-12-15 23:58:08 +0530

    First Commit With Animal Sounds

---
+++ index.js
@@ -0,0 +1,28 @@
+function button1() {
+  var ton1 = new Audio("sounds/dog.wav");
+  ton1.play();
+}
+function button2() {
+  var ton1 = new Audio("sounds/horse.wav");
+  ton1.play();
+}
+function button3() {
+  var ton1 = new Audio("sounds/lion.wav");
+  ton1.play();
+}
+function button4() {
+  var ton1 = new Audio("sounds/cat.wav");
+  ton1.play();
+}
+function button5() {
+  var ton1 = new Audio("sounds/monkey.wav");
+  ton1.play();
+}
+function button6() {
+  var ton1 = new Audio("sounds/bees.wav");
+  ton1.play();
+}
+function button7() {
+  var ton1 = new Audio("sounds/birds.wav");
+  ton1.play();
+}
